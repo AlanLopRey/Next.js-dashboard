b385d86fce4234a2fa0b4b46067ce90623eebc3b
Alan Lopez <alan_lopezrey9822@outlook.com>
2024-11-29 02:53:25 -0600

    chapter 9 streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,13 +13,13 @@ export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    const time = 6000;
+    console.log("Fetching revenue data...");
+    await new Promise((resolve) => setTimeout(resolve, time));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log(`Data fetch completed after ${time} mili seconds.`);
 
     return data.rows;
   } catch (error) {
